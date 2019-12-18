8eaa21ed19aab3e297e001a3fd5f66e2aba857b7
bky373 <bo373@naver.com>
2019-12-18 16:38:16 +0900

    p158_View클래스의 onTouchEvent() 사용하기_돈 되는 안드로이드 앱 만들기

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.myfirstapp;
 
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,16 +14,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        MyView myView = new MyView(this);
+        setContentView(myView);
     }
 
-    /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+    class MyView extends View {
+        MyView(Context context) {
+            super(context);
+        }
+        public boolean onTouchEvent (MotionEvent event){
+            //내부 클래스에서 MainActivity 객체를 사용하기 위해서는
+            //MainActivity.this로 사용해야 한다.
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                Toast.makeText(MainActivity.this, "View touch", Toast.LENGTH_SHORT).show();
+            }
+            // 리턴값이 true이면 Activity의 onTouchEvent()가 실행되지 않는다.
+            return false;
+        }
+    }
+
+
+    /**
+     * Called when the user taps the Send button
+     */
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Toast.makeText(this, "Activity touch", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
 
