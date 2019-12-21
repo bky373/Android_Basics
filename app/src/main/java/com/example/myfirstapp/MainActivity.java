67b06916bfa6413e1dc5ab8b89f50ced2820ec56
bky373 <bo373@naver.com>
2019-12-21 18:44:22 +0900

    내부 클래스로 리스너 인터페이스 구현하기_from_돈 되는 안드로이드 앱 만들기_p159

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,37 +1,50 @@
 package com.example.myfirstapp;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MyView myView = new MyView(this);
-        setContentView(myView);
+        setContentView(R.layout.activity_main);
+//        MyView myView = new MyView(this);
+        MyListener mL = new MyListener(); // 이벤트 리스너 객체 생성하기
+
+        b1 = findViewById(R.id.b1);
+        b1.setOnClickListener(mL); // 위젯에 이벤트 리스너 객체 등록하기
+
     }
 
-    class MyView extends View {
-        MyView(Context context) {
-            super(context);
-        }
-        public boolean onTouchEvent (MotionEvent event){
-            //내부 클래스에서 MainActivity 객체를 사용하기 위해서는
-            //MainActivity.this로 사용해야 한다.
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                Toast.makeText(MainActivity.this, "View touch", Toast.LENGTH_SHORT).show();
-            }
-            // 리턴값이 true이면 Activity의 onTouchEvent()가 실행되지 않는다.
-            return false;
+    // 인터페이스 OnClickListener를 구현한 클래스를 만든다.
+    // onClick 메소드를 구현해준다.
+    class MyListener implements View.OnClickListener {
+        
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "리스너 객체를 사용한 이벤트 처리", Toast.LENGTH_SHORT).show();
         }
     }
+//    class MyView extends View {
+//        MyView(Context context) {
+//            super(context);
+//        }
+//        public boolean onTouchEvent (MotionEvent event){
+//            //내부 클래스에서 MainActivity 객체를 사용하기 위해서는
+//            //MainActivity.this로 사용해야 한다.
+//            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                Toast.makeText(MainActivity.this, "View touch", Toast.LENGTH_SHORT).show();
+//            }
+//            // 리턴값이 true이면 Activity의 onTouchEvent()가 실행되지 않는다.
+//            return false;
+//        }
+//    }
 
 
     /**
@@ -45,14 +58,12 @@ public class MainActivity extends AppCompatActivity {
 //        startActivity(intent);
 //    }
 
-    public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            Toast.makeText(this, "Activity touch", Toast.LENGTH_SHORT).show();
-        }
-        // TODO 내일 2019년 12월 19일에는 꼭 올리자
-        // TODO 왜 오늘 안올렸니ㅠㅠㅠ 2019년 12월 20일 내일 꼭 올리자!
-        return false;
-    }
+//    public boolean onTouchEvent(MotionEvent event) {
+//        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//            Toast.makeText(this, "Activity touch", Toast.LENGTH_SHORT).show();
+//        }
+//        return false;
+//    }
 }
 
 
