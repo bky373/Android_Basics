b4f7970b4cbeb34d487363056dc23c5e80a81f22
bky373 <bo373@naver.com>
2019-12-25 22:42:53 +0900

    191225_TODO까지 설정 완료"

---
+++ app/src/main/java/com/example/myfirstapp/ShowDiceApp.java
@@ -0,0 +1,17 @@
+package com.example.myfirstapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowDiceApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diceapp);
+
+        // Todo Chapter 5까지 실습해보기
+    }
+}
